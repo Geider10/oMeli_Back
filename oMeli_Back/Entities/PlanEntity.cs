d14e3079fa37ee4e9b3e684085c993f8e99bbeb0
Geider Frias <geiderde20@gmail.com>
2025-04-21 01:04:00 -0300

    feat(subscription): added PlanEntity

---
+++ oMeli_Back/Entities/PlanEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace oMeli_Back.Entities
+{
+    public class PlanEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int PublicationLimit { get; set; }
+        public bool ImportCSV { get; set; }
+        public bool StoreCustom { get; set; }
+        public bool ViewStatics { get; set; }
+    }
+}
