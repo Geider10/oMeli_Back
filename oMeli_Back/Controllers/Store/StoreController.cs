f832813ea68d27625d49f7947eb1767c51450a15
Geider Frias <geiderde20@gmail.com>
2025-05-03 17:22:06 -0300

    feat(store): added create store funtionality

---
+++ oMeli_Back/Controllers/Store/StoreController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using oMeli_Back.Services.Store;
+using oMeli_Back.DTOs.Store;
+using oMeli_Back.Validators.Store;
+using FluentValidation.Results;
+
+namespace oMeli_Back.Controllers.Store
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private StoreService _storeService;
+        public StoreController(StoreService storeService)
+        {
+            _storeService = storeService;
+        }
+
+        [HttpPost][Route("")]
+        public async Task<ActionResult> CreateStore([FromBody] CreateStoreDto storeDto)
+        {
+            try
+            {
+                ValidationResult validateCreateStore = new CreateStoreValidator().Validate(storeDto);
+                if (!validateCreateStore.IsValid) return BadRequest(validateCreateStore.Errors);
+
+                var res = await _storeService.CreateStore(storeDto);
+                return CreatedAtAction(nameof(CreateStore), res);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ oMeli_Back/Services/Store/StoreService.cs
@@ -0,0 +1,38 @@
+﻿using oMeli_Back.Context;
+using oMeli_Back.DTOs;
+using oMeli_Back.DTOs.Store;
+using oMeli_Back.Entities;
+
+namespace oMeli_Back.Services.Store
+{
+    public class StoreService
+    {
+        private AppDBContext _context;
+        public StoreService(AppDBContext context)
+        {
+            _context = context;           
+        }
+
+        public async Task<GeneralRes> CreateStore(CreateStoreDto storeDto)
+        {
+            var store = new StoreEntity
+            {
+                UserId = Guid.Parse(storeDto.UserId),
+                SubscriptionId = Guid.Parse(storeDto.SubscriptionId),
+                Name = storeDto.Name,
+                Wassap = storeDto.Wassap,
+                Mail = storeDto.Mail,
+                HasLocal = storeDto.HasLocal,
+                Address = storeDto.Address,
+                AddressDescription = storeDto.AddressDescription,
+                LocalNumber = storeDto.LocalNumber,
+                CurrentProducts = storeDto.CurrentProducts
+            };
+            await _context.Stores.AddAsync(store);
+            await _context.SaveChangesAsync();
+
+            return new GeneralRes { Ok = true, Message = "Store created" };
+        }
+
+    }
+}
